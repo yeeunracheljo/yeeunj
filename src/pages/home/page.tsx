8dff5adccfd0562e939bb42c7b5beefb6fee685d
yeeunracheljo <yeeunrchl@gmail.com>
2026-05-04 22:12:58 +0900

    Raise stats band

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       <main>
         <div className="bg-gradient-to-br from-white via-[#f8fbff]/60 to-[#eef5ff]/40">
           <PortfolioHero />
-          <div className="mt-44 md:mt-52">
+          <div className="mt-32 md:mt-40">
             <QuickStats />
           </div>
         </div>
