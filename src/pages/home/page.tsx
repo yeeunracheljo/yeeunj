c8ae6604191e10eea804c5e88be3ed3fa0d628f2
yeeunracheljo <yeeunrchl@gmail.com>
2026-05-04 22:19:19 +0900

    Fine tune stats band position

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       <main>
         <div className="bg-gradient-to-br from-white via-[#f8fbff]/60 to-[#eef5ff]/40">
           <PortfolioHero />
-          <div className="mt-8 md:mt-16">
+          <div className="mt-4 md:mt-10">
             <QuickStats />
           </div>
         </div>
