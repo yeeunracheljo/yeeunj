6921be84a139245665adb801a258e713100d2507
yeeunracheljo <yeeunrchl@gmail.com>
2026-05-04 23:02:46 +0900

    Tiny stats band nudge

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       <main>
         <div className="bg-gradient-to-br from-white via-[#f8fbff]/60 to-[#eef5ff]/40">
           <PortfolioHero />
-          <div className="mt-[4.875rem] md:mt-[6.875rem]">
+          <div className="mt-[4.9375rem] md:mt-[6.9375rem]">
             <QuickStats />
           </div>
         </div>
