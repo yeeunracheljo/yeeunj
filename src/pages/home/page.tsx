881b6e83b08aac01c6f780562531f6c234e564f6
yeeunracheljo <yeeunrchl@gmail.com>
2026-05-04 23:49:13 +0900

    Tiny lower stats band tweak

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       <main>
         <div className="bg-gradient-to-br from-white via-[#f8fbff]/60 to-[#eef5ff]/40">
           <PortfolioHero />
-          <div className="mt-[5.0625rem] md:mt-[7.0625rem]">
+          <div className="mt-[5.125rem] md:mt-[7.125rem]">
             <QuickStats />
           </div>
         </div>
