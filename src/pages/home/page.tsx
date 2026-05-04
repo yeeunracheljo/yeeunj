d3b304483a9aa24118d6de103e18e3634ad5f7f9
yeeunracheljo <yeeunrchl@gmail.com>
2026-05-05 00:05:48 +0900

    Tiny stats band position tweak

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       <main>
         <div className="bg-gradient-to-br from-white via-[#f8fbff]/60 to-[#eef5ff]/40">
           <PortfolioHero />
-          <div className="mt-[5.234375rem] md:mt-[7.234375rem]">
+          <div className="mt-[5.25rem] md:mt-[7.25rem]">
             <QuickStats />
           </div>
         </div>
