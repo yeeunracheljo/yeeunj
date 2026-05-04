ecce3b291b89d2af43598f4c187f9b2ab6d31663
yeeunracheljo <yeeunrchl@gmail.com>
2026-05-04 22:32:49 +0900

    Micro adjust stats band

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       <main>
         <div className="bg-gradient-to-br from-white via-[#f8fbff]/60 to-[#eef5ff]/40">
           <PortfolioHero />
-          <div className="mt-16 md:mt-24">
+          <div className="mt-[4.25rem] md:mt-[6.25rem]">
             <QuickStats />
           </div>
         </div>
