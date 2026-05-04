772e0e0fba02c500de7390971abf5a40a734ef5c
yeeunracheljo <yeeunrchl@gmail.com>
2026-05-04 21:57:50 +0900

    Move stats band below fold

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       <main>
         <div className="bg-gradient-to-br from-white via-[#f8fbff]/60 to-[#eef5ff]/40">
           <PortfolioHero />
-          <div className="mt-28 md:mt-32">
+          <div className="mt-44 md:mt-52">
             <QuickStats />
           </div>
         </div>
