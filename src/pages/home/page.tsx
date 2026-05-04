a5e55d1129b4bce04e25e4f4c779a8a0bd11e480
yeeunracheljo <yeeunrchl@gmail.com>
2026-05-04 23:52:09 +0900

    Micro nudge stats lower

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       <main>
         <div className="bg-gradient-to-br from-white via-[#f8fbff]/60 to-[#eef5ff]/40">
           <PortfolioHero />
-          <div className="mt-[5.125rem] md:mt-[7.125rem]">
+          <div className="mt-[5.1875rem] md:mt-[7.1875rem]">
             <QuickStats />
           </div>
         </div>
